e0bad5ff5363186b840da8d1f3261103d3d67bac
Chaira <chaira10@hotmail.fr>
2024-02-16 17:51:14 +0100

    correction erreur console datatable

--- src/Components/Datatable/Datatable.jsx
+++ src/Components/Datatable/Datatable.jsx
@@ -2,6 +2,11 @@ import  './Datatable.css';
 import  DataTable from 'react-data-table-component';
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
+import styled from 'styled-components';
+
+const StyledDataTable = styled(DataTable)`
+  display: none;
+`;
 
 function Datatable() {
     const employeesData = useSelector(state => state.data.employees);
@@ -10,46 +15,55 @@ function Datatable() {
             name: 'First Name',
             selector: row => row.firstName,
             sortable: true,
+            reorder: true,
         },
         {
             name: 'Last Name',
             selector: row => row.lastName,
             sortable: true,
+            reorder: true,
         },
         {
             name: 'Start Date',
             selector: row => row.startDate,
             sortable: true,
+            reorder: true,
         },
         {
             name: 'Department',
             selector: row => row.department,
             sortable: true,
+            reorder: true,
         },
         {
             name: 'Date of Birth',
             selector: row => row.dateOfBirth,
             sortable: true,
+            reorder: true,
         },
         {
             name: 'Street',
             selector: row => row.street,
             sortable: true,
+            reorder: true,
         },
         {
             name: 'City',
             selector: row => row.city,
             sortable: true,
+            reorder: true,
         },
         {
             name: 'State',
             selector: row => row.state,
             sortable: true,
+            reorder: true,
         },
         {
             name: 'State',
             selector: row => row.zipCode,
             sortable: true,
+            reorder: true,
         },
     ];
 
@@ -67,8 +81,8 @@ function Datatable() {
     <div className="text-end">
         <input type="text" onChange={handleFilter}/>
     </div>
-    <DataTable columns={columns} data={records} fixedHeader pagination striped>
-    </DataTable>
+    <DataTable columns={columns} data={records} fixedHeader pagination striped pointerOnHover />
+    <StyledDataTable sortActive={true} />
     </div>
     )
 }
